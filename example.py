1611f7b120b7cb6d24b30ef2fa8e75b2914defac
zhujinxuan <zhujinxuan@gmail.com>
2024-05-14 10:54:48 -0400

    Upgrade pyproject

--- example.py
+++ example.py
@@ -1,15 +1,19 @@
 from build123d import *
 from yacv_server import show
 
-a, b = 40, 20
+length, thickness = 80.0, 10.0
 
-with BuildPart() as ex14:
-    with BuildLine() as ex14_ln:
-        l1 = JernArc(start=(0, 0), tangent=(0, 1), radius=a, arc_size=180)
-        l2 = JernArc(start=l1 @ 1, tangent=l1 % 1, radius=a, arc_size=-90)
-        l3 = Line(l2 @ 1, l2 @ 1 + Vector(-a, a))
-    with BuildSketch(Plane.XZ) as ex14_sk:
-        Rectangle(b, b)
-    sweep()
+with BuildPart() as ex19:
+    with BuildSketch() as ex19_sk:
+        RegularPolygon(radius=length / 2, side_count=7)
+    extrude(amount=thickness)
+    topf = ex19.faces().sort_by(Axis.Z)[-1]
+    vtx = topf.vertices().sort_by(Axis.X)[-1]
+    vtx2Axis = Axis((0, 0, 0), (-1, -0.5, 0))
+    vtx2 = topf.vertices().sort_by(vtx2Axis)[-1]
+    with BuildSketch(topf) as ex19_sk2:
+        with Locations((vtx.X, vtx.Y), (vtx2.X, vtx2.Y)):
+            Circle(radius=length / 8)
+    extrude(amount=-thickness, mode=Mode.SUBTRACT)
 
-show(ex14)
+show(ex19.part)
